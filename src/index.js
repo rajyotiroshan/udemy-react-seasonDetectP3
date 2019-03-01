192c368695f78ff14b33098aa52a7bd258f2c33c
rajan <rajangreedge@gmail.com>
2019-03-01 10:02:15 +0530

    scaffold project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = ()=>{
+    return (
+        <div>
+            Hi There!.
+        </div>
 
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
 
 
