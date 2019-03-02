83a55feaa568d8d70c3c3be1bf8a0e2ff4dc157a
rajan <rajangreedge@gmail.com>
2019-03-02 19:03:57 +0530

    added style

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -24,7 +24,7 @@ const SeasonDisplay = (props)=>{
     const season = getSeason(props.lat, new Date().getMonth());
     const { text, iconName} = seasonConfig[season];
     return (
-        <div>
+        <div className={`season-display ${season}`}>
             <i className={`icon-left massive ${iconName} icon `}/>
             <h1>{text}</h1>
             <i className={` icon-right massive ${iconName} icon`}/>
