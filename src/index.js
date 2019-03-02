b156068b7c071223c4460f934841cc2298909a22
rajan <rajangreedge@gmail.com>
2019-03-02 12:15:57 +0530

    logic gor determining season

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   //first method
@@ -29,7 +30,7 @@ class App extends React.Component {
     }
 
     if( !this.state.errMessage && this.state.lat) {
-        return <div>Latitude: {this.state.lat}</div>
+        return <SeasonDisplay  lat={this.state.lat}/>
     }
 
     return (
